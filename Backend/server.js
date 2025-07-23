ec576ef90a4a05c02434fe08f48207df770d9e63
Devendra kushwah <devendrakus2005@gmail.com>
2025-07-23 13:30:49 +0530

    CORS solve

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@
     
     server.use(cookieParser(    ))
 const allowedOrigins = [
-  "https://trackit-frontend-b0ed.onrender.com/",
+  "https://trackit-frontend-b0ed.onrender.com",
   "http://localhost:3000", 
 ];
 
